4ede947730e297072fb4bdc86a815666bd06c573
Amogh Rathore <amoghdroid09@gmail.com>
2017-06-25 11:21:34 +0900

    Added reversal in groups

--- linked_list.py
+++ linked_list.py
@@ -3,6 +3,91 @@ class Node(object):
         self.val = val
         self.next = next
 
+    def reverse(self):
+        """Reverses the list with node as head and
+        returns the head to the reversed list"""
+        if self == None:
+            return None
+        a = None
+        n = self
+        b = self.next
+        while True:
+            n.next = a
+            a = n
+            n = b
+            if b != None:
+                b = b.next
+            else:
+                break
+        return a
+
+    def merge(h1, h2):
+        sorted = LinkedList()
+        while h1 != None or h2 != None:
+            if h1 != None and h2 != None:
+                if h1.val <= h2.val:
+                    small = h1
+                    h1 = h1.next
+                else:
+                    small = h2
+                    h2 = h2.next
+            elif h1 == None:
+                small = h2
+                h2 = h2.next
+            else:
+                small = h1
+                h1 = h1.next
+            small.next = None
+            sorted.insert_at_end(small)
+        return sorted
+
+    def find_middle(self):
+        if self == None:
+            raise ValueError("Passed node is None")
+        prev = None
+        fast = slow = self
+        while fast != None and fast.next != None:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        return (prev, slow)
+
+    def find_kth_node(self, k):
+        if self == None:
+            raise ValueError("This node is None")
+        n = self
+        for i in range(k-1):
+            try:
+                n = n.next
+                if n == None:
+                    raise AttributeError("")
+            except AttributeError:
+                raise IndexError("Index out of bounds")
+        return n
+
+    def reverse_groups(self, k, prev=None):
+        """Reverses list in groups of k nodes"""
+        if self == None:
+            return None
+
+        try:
+            node_k = self.find_kth_node(k)
+        except IndexError:
+            return None
+        node_k_next = node_k.next
+
+        if prev != None:
+            prev.next = None
+        node_k.next = None
+
+        r_head = self.reverse()
+        if prev != None:
+            prev.next = r_head
+        self.next = node_k_next
+        if node_k_next != None:
+            node_k_next.reverse_groups(k, self)
+        return r_head
+
     def __str__(self):
         return str(self.val)
 
@@ -103,18 +188,24 @@ class LinkedList(object):
         """Reverses the linked list"""
         if self.head == None:
             return
-        a = None
-        n = self.head
-        b = n.next
-        while True:
-            n.next = a
-            a = n
-            n = b
-            if b != None:
-                b = b.next
-            else:
-                break
-        self.head = a
+        self.head = Node.reverse(self.head)
+
+    def reverse_groups(self, k):
+        self.head.find_kth_node(k)    
+        self.head = self.head.reverse_groups(k)
+
+    def merge_sort(self):
+        if self.head == None:
+            return
+        if self.head.next == None:
+            return
+        prev, middle = Node.find_middle(self.head)
+        prev.next = None
+        second_list = LinkedList(middle)
+        self.merge_sort()
+        second_list.merge_sort()
+        sorted = Node.merge(self.head, second_list.head)
+        self.head = sorted.head
 
     def __str__(self):
         n = self.head
@@ -123,28 +214,6 @@ class LinkedList(object):
             n = n.next
             ret += " --> " + str(n)
         return ret
-
-    def merge(list1, list2):
-        sorted = LinkedList()
-        h1 = list1.head
-        h2 = list2.head
-        while h1 != None or h2 != None:
-            if h1 != None and h2 != None:
-                if h1.val <= h2.val:
-                    small = h1
-                    h1 = h1.next
-                else:
-                    small = h2
-                    h2 = h2.next
-            elif h1 == None:
-                small = h2
-                h2 = h2.next
-            else:
-                small = h1
-                h1 = h1.next
-            small.next = None
-            sorted.insert_at_end(small)
-        return sorted 
 
 class NotFoundError(Exception):
     pass
