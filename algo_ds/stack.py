c65f7dc198c63854bf4b53094786b4240563aa52
Amogh Rathore <amoghdroid09@gmail.com>
2017-07-07 23:59:39 +0900

    merge sort for stack

--- algo_ds/stack.py
+++ algo_ds/stack.py
@@ -26,5 +26,48 @@ class Stack(object):
         stack2.push(stack.pop())
         return Stack.reverse_rec(stack, stack2)
 
+    def merge(s1, s2):
+        if (s1 == None):
+            return s2
+        if (s2 == None):
+            return s1
+        stack = Stack()
+        while not (s1.isEmpty() and s2.isEmpty()):
+            if not (s1.isEmpty() or s2.isEmpty()):
+                if s1.peek() <= s2.peek():
+                    stack.push(s2.pop())
+                else:
+                    stack.push(s1.pop())
+            elif not s1.isEmpty():
+                stack.push(s1.pop())
+            else:
+                stack.push(s2.pop())
+        return stack.reverse()
+
+    def split(stack):
+        count = 0
+        tmp = Stack()
+        while not stack.isEmpty():
+            tmp.push(stack.pop())
+            count += 1
+        i = 0
+        for i in range(count//2):
+            stack.push(tmp.pop())
+        return (stack, tmp)
+
+    def merge_sort(stack):
+        if stack.isEmpty():
+            return stack
+        elem = stack.pop()
+        if stack.isEmpty():
+            stack.push(elem)
+            return stack
+        stack.push(elem)
+
+        s1, s2 = stack.split()
+        s1 = Stack.merge_sort(s1)
+        s2 = Stack.merge_sort(s2)
+        return Stack.merge(s1, s2)
+
     def __str__(self):
         return str(self.list)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -23,5 +23,36 @@ class TestStack(unittest.TestCase):
         stack.push(4)
         self.assertEqual(str(Stack.reverse(stack)), "1 -> 2 -> 3 -> 4 -> None")
 
+    def test_merge(self):
+        s1 = Stack()
+        s1.push(1)
+        s1.push(3)
+        s1.push(5)
+        s1.push(7)
+        s2 = Stack()
+        s2.push(2)
+        s2.push(4)
+        self.assertEqual(str(Stack.merge(s1, s2)), "7 -> 5 -> 4 -> 3 -> 2 -> 1 -> None")
+
+    def test_split(self):
+        s = Stack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        s.push(4)
+        s.push(5)
+        s1, s2 = s.split()
+        self.assertEqual(str(s1), "2 -> 1 -> None")
+        self.assertEqual(str(s2), "3 -> 4 -> 5 -> None")
+
+    def test_merge_sort(self):
+        s = Stack()
+        s.push(3)
+        s.push(1)
+        s.push(5)
+        s.push(2)
+        s.push(4)
+        self.assertEqual(str(Stack.merge_sort(s)), "5 -> 4 -> 3 -> 2 -> 1 -> None")
+
 if __name__ == '__main__':
     unittest.main()
