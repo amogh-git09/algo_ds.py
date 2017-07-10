a505541ae5ba0feae7a5649815566e3d8f652ffd
Amogh Rathore <amoghdroid09@gmail.com>
2017-07-10 23:18:20 +0900

    queue using two stacks

---
+++ algo_ds/queue_using_stacks.py
@@ -0,0 +1,20 @@
+from algo_ds.stack import Stack
+
+class QueueUsingStacks(object):
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, val):
+        self.stack1.push(val)
+
+    def dequeue(self):
+        if self.stack1.isEmpty() and self.stack2.isEmpty():
+            raise IndexError("Queue empty")
+
+        # pour stack1 into stack2
+        if self.stack2.isEmpty():
+            while not self.stack1.isEmpty():
+                self.stack2.push(self.stack1.pop())
+
+        return self.stack2.pop()
