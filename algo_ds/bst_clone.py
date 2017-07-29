1ad784390a0b8b7fceb8a51ffae3f7d42208d34e
Amogh Rathore <amoghdroid09@gmail.com>
2017-07-29 11:34:20 +0900

    added bst with random pointer clone without map

--- algo_ds/bst_clone.py
+++ algo_ds/bst_clone.py
@@ -26,3 +26,40 @@ class Node(object):
             clone_node.random = hashmap[tree_node.random]
         Node.copy_random(clone_node.left, tree_node.left, hashmap)
         Node.copy_random(clone_node.right, tree_node.right, hashmap)
+
+    def copy_left_right_nomap(tree_node):
+        if tree_node is None:
+            return
+        clone_node = Node(tree_node.key, tree_node.val)
+        clone_node.left = tree_node.left
+        tree_node.left = clone_node
+        Node.copy_left_right_nomap(clone_node.left)
+        Node.copy_left_right_nomap(tree_node.right)
+        if tree_node.right is not None:
+            clone_node.right = tree_node.right.left
+
+    def copy_random_nomap(tree_node):
+        if tree_node is None:
+            return
+        clone_node = tree_node.left
+        if tree_node.random is not None:
+            clone_node.random = tree_node.random.left
+        Node.copy_random_nomap(clone_node.left)
+        Node.copy_random_nomap(tree_node.right)
+
+    def decouple(tree_node):
+        if tree_node is None:
+            return
+        clone_node = tree_node.left
+        tree_node.left = clone_node.left
+        if tree_node.left is not None:
+            clone_node.left = tree_node.left.left
+        Node.decouple(tree_node.left)
+        Node.decouple(tree_node.right)
+        return clone_node
+
+    def clone_nomap(self):
+        self.copy_left_right_nomap()
+        self.copy_random_nomap()
+        clone = self.decouple()
+        return clone
