dec1c64b76de41c64143e814f39918a7d91a448d
Amogh Rathore <amoghdroid09@gmail.com>
2017-08-12 01:40:00 +0900

    added some heap functions

--- algo_ds/binary_heap.py
+++ algo_ds/binary_heap.py
@@ -1,7 +1,16 @@
+from functools import total_ordering
+
+@total_ordering
 class Element(object):
     def __init__(self, key, val=None):
         self.key = key
         self.val = val
+
+    def __eq__(self, other):
+        return self.key == other.key
+
+    def __lt__(self, other):
+        return self.key < other.key
 
     def __str__(self):
         return ("<key: {}, val: {}>".format(self.key, self.val))
@@ -25,6 +34,41 @@ class BinaryHeap(object):
             self.swap(p, index)
             index = p
             p = self.parent(index)
+
+    def heapify(self, index=0):
+        c1 = self.left_child(index)
+        c2 = self.right_child(index)
+        if c1 >= self.size:
+            return
+        min_index = index
+        if self.arr[min_index] > self.arr[c1]:
+            min_index = c1
+        if c2 < self.size and self.arr[min_index] > self.arr[c2]:
+            min_index = c2
+        if min_index != index:
+            self.swap(min_index, index)
+            self.heapify(min_index)
+
+    def get_min(self):
+        if self.size <= 0:
+            raise IndexError("Heap empty")
+        return self.arr[0]
+
+    def extract_min(self):
+        if self.size <= 0:
+            raise IndexError("Heap empty")
+        self.swap(0, self.size-1)
+        self.size -= 1
+        self.heapify()
+        return self.arr[self.size]
+
+    @staticmethod
+    def left_child(index):
+        return 2*index + 1
+
+    @staticmethod
+    def right_child(index):
+        return 2*index + 2
 
     @staticmethod
     def parent(index):
--- tests/test_binary_heap.py
+++ tests/test_binary_heap.py
@@ -1,5 +1,5 @@
 import unittest
-from algo_ds.binary_heap import BinaryHeap
+from algo_ds.binary_heap import BinaryHeap, Element
 
 class TestBinaryHeap(unittest.TestCase):
     def test_insert(self):
@@ -15,3 +15,39 @@ class TestBinaryHeap(unittest.TestCase):
         heap.insert(75)
         heap.insert(70)
         self.assertRaises(IndexError, heap.insert, 80)
+
+    def test_heapify(self):
+        heap = BinaryHeap(8)
+        heap.arr = [Element(x) for x in [4,7,1,3,2,8,5,6]]
+        heap.size = len(heap.arr)
+        for i in range(0, len(heap.arr)//2):
+            heap.heapify(i)
+        self.assertEqual(heap.get_keys(), [1,2,4,3,7,8,5,6])
+
+    def test_get_min(self):
+        heap = self.get_test_heap()
+        self.assertEqual(heap.get_min().key, 20)
+        self.assertEqual(heap.get_min().key, 20)
+
+    def test_extract_min(self):
+        heap = self.get_test_heap()
+        size = heap.size
+        self.assertEqual(heap.extract_min().key, 20)
+        self.assertEqual(heap.size, size-1)
+        self.assertEqual(heap.extract_min().key, 30)
+        self.assertEqual(heap.extract_min().key, 40)
+        self.assertEqual(heap.extract_min().key, 50)
+        self.assertEqual(heap.extract_min().key, 60)
+        self.assertEqual(heap.extract_min().key, 80)
+        self.assertRaises(IndexError, heap.extract_min)
+
+    @staticmethod
+    def get_test_heap():
+        heap = BinaryHeap(8)
+        heap.insert(50)
+        heap.insert(60)
+        heap.insert(20)
+        heap.insert(40)
+        heap.insert(30)
+        heap.insert(80)
+        return heap
